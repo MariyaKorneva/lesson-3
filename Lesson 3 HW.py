0be0b55eda32df1ed9b62896d65bb791781edb66
MariyaKorneva <85699755+MariyaKorneva@users.noreply.github.com>
2022-02-07 13:28:02 +0300

    Add files via upload

---
+++ Lesson 3 HW.py
@@ -0,0 +1,11 @@
+print("Введите значение x")
+x=int(input("x="))
+if x>0:
+    y=2*x-10
+    print(y)
+elif x==0: 
+    y==0
+    print(0)
+else:
+    y=2*x-1
+    print(y)
---
+++ lesson3.py
@@ -0,0 +1,53 @@
+#import math
+#print('Решим квадратное уравнение ax^2+bx+c=0')
+#a=int(input('a='))
+#b=int(input('b='))
+#c=int(input('c='))
+#d=b*b-4*a*c
+#if d>0:
+    #x1=(-b+math.sqrt(d))/2*a
+    #x2=(-b-math.sqrt(d))/2*a
+    #print(f"первый корень{x1},\nвторой корень{x2}")
+#elif d==0:
+    #x=-b/2*a
+    #print(f"Корень{x}")
+#else:
+    #print(f"Кореней нет")
+
+#a=int(input("pass."))
+#b=8484
+#if a==b:
+    #print("Твой пароль")
+#else:
+    #print("Er")
+
+#n=int(input("n:"))
+#if n%2==0:
+    #print("Я 2й")
+#else:
+    #print("Я 1й")
+
+#a=int(input("a:"))
+#b=int(input("b:"))
+#c=int(input("c:"))
+#if((a+b)<c)or((a+c)<b)or((b+c)<a):
+    #print("Треугольник не существует")
+#else:
+    #if a==b and b==c:
+        #print("Треугольник Равносторонний")
+    #elif a==b or b==c:
+        #print("Равнобедренный треугольник")
+    #else:
+        #print("Разносторонний треугольник") 
+
+#print("введите координаты точки A(x;y)")
+#x=int(input("x="))
+#y=int(input("y="))
+#if x>0 and y>0:
+    #print("в 1 четверти")
+#elif x<0 and y>0:#
+    #print("в 2 четверти")
+#elif x<0 and y<0:#
+    #print("в 3 четверти")
+#else:
+    #print("в 4 четверти")
